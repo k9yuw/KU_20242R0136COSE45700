a319b142f907d83579e7134708bc0d8af525962b
k9yuw <gyuwoneee@gmail.com>
2024-11-30 03:47:39 +0900

    feat: MainView 추가 #1

---
+++ miridih_canvas/src/view/ButtonPanel.java
@@ -0,0 +1,14 @@
+package view;
+
+import controller.Controller;
+import model.ShapeManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonPanel extends JPanel {
+
+    public ButtonPanel(Controller controller, ShapeManager shapeManager) {
+
+    }
+}
--- miridih_canvas/src/view/MainView.java
+++ miridih_canvas/src/view/MainView.java
@@ -1,4 +1,42 @@
 package view;
 
-public class MainView {
+import controller.Controller;
+import model.ShapeManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView extends JFrame {
+    private CanvasPanel canvasPanel;
+    private PropertyPanel propertyPanel;
+    private ButtonPanel buttonPanel;
+    private ShapeManager shapeManager;
+
+    public MainView(ShapeManager shapeManager) {
+        canvasPanel = new CanvasPanel(shapeManager);
+        this.shapeManager = shapeManager;
+    }
+
+    public void initialize(Controller controller) {
+        setLayout(new BorderLayout());
+        setTitle("FULL HOUSE");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // CanvasPanel 설정
+        canvasPanel.setBackground(Color.WHITE);
+        canvasPanel.addMouseListener(controller);
+        canvasPanel.addMouseMotionListener(controller);
+        add(canvasPanel, BorderLayout.CENTER);
+
+        // PropertyPanel 추가
+        propertyPanel = new PropertyPanel(controller, shapeManager);
+        add(propertyPanel, BorderLayout.EAST);
+
+        // ButtonPanel 추가
+        buttonPanel = new ButtonPanel(controller, shapeManager);
+        add(buttonPanel, BorderLayout.NORTH);
+
+        setVisible(true);
+    }
 }
