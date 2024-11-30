18e3d4c68f01ef2ac7cf35d01bb41184c2432517
k9yuw <gyuwoneee@gmail.com>
2024-11-30 17:24:25 +0900

    feat: ImageShape 작성 #2

---
+++ miridih_canvas/src/model/shape/ImageShape.java
@@ -0,0 +1,68 @@
+package model.shape;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageShape extends AbstractShape {
+
+    private int x, y, width, height;
+    private BufferedImage image;
+
+    public ImageShape(int x1, int y1, int x2, int y2, Color color, BufferedImage image) {
+        super(x1, y1, x2, y2, color);
+
+        this.x = Math.min(x1, x2);
+        this.y = Math.min(y1, y2);
+        this.width = Math.abs(x2 - x1);
+        this.height = Math.abs(y2 - y1);
+        this.image = image;
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        if (image != null) {
+            // 이미지를 x, y 좌표에 width x height 크기로 그립니다.
+            g2.drawImage(image, x, y, width, height, null);
+        }
+    }
+
+    @Override
+    public String getX() {
+        return String.valueOf(x);
+    }
+
+    @Override
+    public String getY() {
+        return String.valueOf(y);
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String getWidth() {
+        return String.valueOf(width);
+    }
+
+    @Override
+    public String getHeight() {
+        return String.valueOf(height);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
